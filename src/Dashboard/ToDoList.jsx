f30700cd48d1c4b373f202b73618b9e625e4a304
Shawon Mahmud <shawon.phero@gmail.com>
2023-12-25 22:26:54 +0600

    delete todo function added

--- src/Dashboard/ToDoList.jsx
+++ src/Dashboard/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { LuPlus } from "react-icons/lu";
 // React Hook Form
 import { useForm } from "react-hook-form";
@@ -17,34 +17,75 @@ import {
   Typography,
   Input,
 } from "@material-tailwind/react";
-
 import Swal from "sweetalert2";
 import { AuthContext } from "../Providers/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const ToDoList = () => {
   const { user } = useContext(AuthContext);
   const [open, setOpen] = React.useState(false);
+  const [openEdit, setOpenEdit] = React.useState(false);
   const handleOpen = () => setOpen((cur) => !cur);
+  const handleOpenEdit = () => setOpenEdit((cur) => !cur);
+
+  const [singleData, setSingleData] = useState({});
+
+  const handleEditBtn = async (_id) => {
+    try {
+      const response = await fetch(
+        `https://task-nest-server-livid.vercel.app/single-todo?id=${_id}`
+      );
+      const data = await response.json();
+      setSingleData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  console.log(singleData);
+
+  // Delete todo task
+  const handleDeleteBtn = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`https://task-nest-server-livid.vercel.app/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire("Task has been Deleted.", "success");
+              todoTaskRefetch();
+            }
+          });
+      }
+    });
+  };
 
   // get user todo task list
-  const [allToDoTask, setAllToDoTask] = useState([]);
+  const {
+    data: allToDoTask,
+    isLoading: isLoadingFrom,
+    refetch: todoTaskRefetch,
+  } = useQuery({
+    queryFn: async () => {
+      const res = await axios.get(
+        `https://task-nest-server-livid.vercel.app/user-todo?email=${user?.email}`
+      );
+      return res.data;
+    },
 
-  const fetchData = () => {
-    fetch(
-      `https://task-nest-server-livid.vercel.app/user-todo?email=${user?.email}`
-    )
-      .then((res) => res.json())
-      .then((data) => setAllToDoTask(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  };
-
-  useEffect(() => {
-    fetchData();
+    queryKey: ["AllTask", "updateTask"],
   });
-
-  const handleRefetch = () => {
-    fetchData();
-  };
 
   const { register, handleSubmit, reset } = useForm();
   const onSubmit = (data) => {
@@ -74,6 +115,7 @@ const ToDoList = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        todoTaskRefetch();
         if (data.insertedId) {
           Swal.fire({
             title: "success!",
@@ -85,7 +127,9 @@ const ToDoList = () => {
         }
       });
   };
-
+  if (isLoadingFrom) {
+    return <div>loading...</div>;
+  }
   return (
     <div className="w-full h-screen overflow-y-scroll  ">
       {/* Create Task Modal/Form*/}
@@ -151,7 +195,7 @@ const ToDoList = () => {
                 </select>
               </CardBody>
               <CardFooter className="pt-0">
-                <div onClick={handleRefetch} className="text-right">
+                <div className="text-right">
                   <Button
                     type="submit"
                     className="bg-gradient-to-r from-[#6F00CC]  to-[#9000CC]  hover:from-[#6F00CC] px-2 py-2 hover:to-[#9000CC] "
@@ -168,7 +212,7 @@ const ToDoList = () => {
       </div>
 
       {/* Task mainlayout */}
-      <div className=" mt-5 grid grid-cols-3 text-center mx-4 gap-3 ">
+      <div className=" mt-5 grid grid-cols-3 text-center mr-4 gap-3 ">
         {/* to-do */}
         <div className=" bg-gray-500 py-2 rounded-md">
           <h3 className="text-xl font-bold flex justify-center items-center ">
@@ -196,8 +240,19 @@ const ToDoList = () => {
                           Deadline: {todo?.deadline}
                         </h5>
                         <div className="flex gap-3">
-                          <FaEdit />
-                          <ImBin />;
+                          <div
+                            onClick={() => {
+                              handleEditBtn(todo?._id);
+                            }}
+                          >
+                            <FaEdit onClick={handleOpenEdit} title="Edit" />
+                          </div>
+                          <ImBin
+                            title="Delete"
+                            onClick={() => {
+                              handleDeleteBtn(todo?._id);
+                            }}
+                          />
                         </div>
                       </div>
                     </CardBody>
@@ -224,6 +279,74 @@ const ToDoList = () => {
           </h3>
           <hr />
         </div>
+        {/* Dialog for Edit todo task */}
+        <Dialog
+          size="xs"
+          open={openEdit}
+          handler={handleOpenEdit}
+          className="bg-transparent shadow-none"
+        >
+          <Card className="mx-auto w-full max-w-[24rem]">
+            <form>
+              <CardBody className="flex flex-col gap-4">
+                <Typography variant="h4" color="blue-gray">
+                  Create a Task
+                </Typography>
+                <Typography className="-mb-2" variant="h6">
+                  Title
+                </Typography>
+                <Input
+                  {...register("title")}
+                  type="text"
+                  label="Title"
+                  size="lg"
+                  defaultValue={singleData?.title}
+                  required
+                />
+                <Typography className="-mb-2" variant="h6">
+                  Description
+                </Typography>
+                <textarea
+                  {...register("description")}
+                  className="border"
+                  cols="10"
+                  rows="4"
+                  defaultValue={singleData?.description}
+                  required
+                ></textarea>
+                <Typography className="-mb-2" variant="h6">
+                  Deadline
+                </Typography>
+                <input
+                  className="border py-1"
+                  {...register("date")}
+                  type="date"
+                  required
+                />
+                <Typography className="-mb-2" variant="h6">
+                  Priority
+                </Typography>
+                <select {...register("priority")} className="border py-2">
+                  <option value="low">Low</option>
+                  <option value="moderate">Moderate</option>
+                  <option value="high">High</option>
+                </select>
+              </CardBody>
+              <CardFooter className="pt-0">
+                <div className="text-right">
+                  <Button
+                    type="submit"
+                    className="bg-gradient-to-r from-[#6F00CC]  to-[#9000CC]  hover:from-[#6F00CC] px-2 py-2 hover:to-[#9000CC] "
+                    onClick={handleOpenEdit}
+                  >
+                    <LuPlus className="inline pb-1 mr-1 text-xl" />
+                    Save Task
+                  </Button>
+                </div>
+              </CardFooter>
+            </form>
+          </Card>
+        </Dialog>
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,19 @@ import router from "./Routes/Router";
 import { ThemeProvider } from "@material-tailwind/react";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./Providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-      <AuthProvider>
-        <ThemeProvider>
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <ThemeProvider>
+            <RouterProvider router={router} />
+          </ThemeProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </HelmetProvider>
   </React.StrictMode>
 );
